347fd992a022aa978e68624be6ee1662c8acfaf0
Arnau-webDev <arnauma16@gmail.com>
2022-01-31 14:30:22 +0100

    Created enemies spawning off-screen randomly located

--- app.js
+++ app.js
@@ -46,15 +46,36 @@ class Projectile {
     }
 }
 
+class Enemy {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+        this.velocity = velocity
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+    }
+
+    update() {
+        this.draw()
+        this.x = this.x + this.velocity.x
+        this.y = this.y + this.velocity.y
+    }
+}
+
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 
 const player = new Player(x, y, 20, "blue");
 
-// const projectile = new Projectile(canvas.width / 2, canvas.height / 2, 5, "red", { x: 1, y: 1 });
-// const projectile2 = new Projectile(canvas.width / 2, canvas.height / 2, 5, "green", { x: -1, y: -1 });
-
 const projectiles = [];
+const enemies = [];
 
 function animate() {
     requestAnimationFrame(animate); // Same as setInterval
@@ -64,14 +85,47 @@ function animate() {
 
     projectiles.forEach(projectile => {
         projectile.update();
+    });
+
+    enemies.forEach(enemy => {
+        enemy.update();
     })
+};
+
+function spawnEnemies() {
+    setInterval(() => {
+        const radius = Math.random() * (30 - 6) + 6;
+
+        let x;
+        let y;
+
+        if (Math.random() < 0.5) {
+            x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius;
+            y = Math.random() * canvas.height;
+        } else {
+            x = Math.random() * canvas.width;
+            y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius;
+        }
+        const color = "green";
+
+        const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x);
+
+        const velocity = {
+            x: Math.cos(angle),
+            y: Math.sin(angle)
+        }
+
+        console.log("Go");
+        enemies.push(new Enemy(x, y, radius, color, velocity));
+    }, 1000);
 }
 
 animate();
+spawnEnemies();
 
 window.addEventListener("click", (e) => {
     const angle = Math.atan2(e.clientY - canvas.height / 2, e.clientX - canvas.width / 2);
-    console.log(angle);
+
     const velocity = {
         x: Math.cos(angle),
         y: Math.sin(angle)
